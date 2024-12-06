78faf6f26f2b94b92b9e668328c7138dd134bcd6
Georgije Stanisic <georgijestanisic@conversifai.co>
2024-12-05 21:49:44 -0700

    initial commit; working but getting rate request errors after a few pulls.

---
+++ cities/cities/spiders/cities_data.py
@@ -0,0 +1,95 @@
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+import pandas as pd
+import json
+
+
+class CitiesDataSpider(CrawlSpider):
+    name = "cities-data"
+    allowed_domains = ["www.city-data.com"]
+    start_urls = [
+        "https://www.city-data.com/city/Alabama.html",
+        "https://www.city-data.com/city/Alaska.html",
+        "https://www.city-data.com/city/Arizona.html",
+        "https://www.city-data.com/city/Arkansas.html",
+        "https://www.city-data.com/city/California.html",
+        "https://www.city-data.com/city/Colorado.html",
+        "https://www.city-data.com/city/Connecticut.html",
+        "https://www.city-data.com/city/Delaware.html",
+        "https://www.city-data.com/city/Florida.html",
+        "https://www.city-data.com/city/Georgia.html",
+        "https://www.city-data.com/city/Hawaii.html",
+        "https://www.city-data.com/city/Idaho.html",
+        "https://www.city-data.com/city/Illinois.html",
+        "https://www.city-data.com/city/Indiana.html",
+        "https://www.city-data.com/city/Iowa.html",
+        "https://www.city-data.com/city/Kansas.html",
+        "https://www.city-data.com/city/Kentucky.html",
+        "https://www.city-data.com/city/Louisiana.html",
+        "https://www.city-data.com/city/Maine.html",
+        "https://www.city-data.com/city/Maryland.html",
+        "https://www.city-data.com/city/Massachusetts.html",
+        "https://www.city-data.com/city/Michigan.html",
+        "https://www.city-data.com/city/Minnesota.html",
+        "https://www.city-data.com/city/Mississippi.html",
+        "https://www.city-data.com/city/Missouri.html",
+        "https://www.city-data.com/city/Montana.html",
+        "https://www.city-data.com/city/Nebraska.html",
+        "https://www.city-data.com/city/Nevada.html",
+        "https://www.city-data.com/city/New-Hampshire.html",
+        "https://www.city-data.com/city/New-Jersey.html",
+        "https://www.city-data.com/city/New-Mexico.html",
+        "https://www.city-data.com/city/New-York.html",
+        "https://www.city-data.com/city/North-Carolina.html",
+        "https://www.city-data.com/city/North-Dakota.html",
+        "https://www.city-data.com/city/Ohio.html",
+        "https://www.city-data.com/city/Oklahoma.html",
+        "https://www.city-data.com/city/Oregon.html",
+        "https://www.city-data.com/city/Pennsylvania.html",
+        "https://www.city-data.com/city/Rhode-Island.html",
+        "https://www.city-data.com/city/South-Carolina.html",
+        "https://www.city-data.com/city/South-Dakota.html",
+        "https://www.city-data.com/city/Tennessee.html",
+        "https://www.city-data.com/city/Texas.html",
+        "https://www.city-data.com/city/Utah.html",
+        "https://www.city-data.com/city/Vermont.html",
+        "https://www.city-data.com/city/Virginia.html",
+        "https://www.city-data.com/city/Washington.html",
+        "https://www.city-data.com/city/West-Virginia.html",
+        "https://www.city-data.com/city/Wisconsin.html",
+        "https://www.city-data.com/city/Wyoming.html"
+    ]
+
+
+    rules = (
+        Rule(LinkExtractor(allow=r"https://www\.city-data\.com/city/[^/]+-[A-Za-z]+\.html"),
+             callback="parse_city", follow=True),
+    )
+
+    def parse_item(self, response):
+        city_state = response.xpath("//h1[@class='city']/span/text()").extract_first() # example Milwaukee, WI
+        table_crimes = response.xpath('//table[@class="table tabBlue tblsort tblsticky sortable"]').extract_first()
+
+        if table_crimes:
+            crimes_df = pd.read_html(table_crimes)[0]
+            crime_data = {
+                'city_state': city_state,
+                'crime_stats': crimes_df.to_dict(orient='records')
+            }
+
+            yield crime_data
+
+    def close(self, reason):
+        # This method is called when the spider finishes executing
+        # It gathers all items and writes to a JSON file
+        all_city_data = []
+
+        # Open the file to write JSON
+        with open('city_crime_data.json', 'w') as f:
+            for item in self.crawled_items:
+                all_city_data.append(item)
+            
+            # Dump all city crime data to a JSON file
+            json.dump(all_city_data, f, indent=4)
